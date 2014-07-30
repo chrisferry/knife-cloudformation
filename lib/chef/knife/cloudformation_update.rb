dc4c9ff12eaa68e0f28ebd334a12b5afd849652b
Chris Ferry <chris.ferry@samsung.com>
2014-07-30 11:43:30 -0400

    Add run method to update

--- lib/chef/knife/cloudformation_update.rb
+++ lib/chef/knife/cloudformation_update.rb
@@ -9,6 +9,48 @@ class Chef
       include KnifeCloudformation::KnifeBase
       include CloudformationCreate::Options
 
+      def run
+        @action_type = self.class.name.split('::').last.sub('Cloudformation', '').upcase
+        name = name_args.first
+        unless(name)
+          ui.fatal "Formation name must be specified!"
+          exit 1
+        end
+ 
+        unless(Chef::Config[:knife][:cloudformation][:template])
+          set_paths_and_discover_file!
+          unless(File.exists?(Chef::Config[:knife][:cloudformation][:file].to_s))
+            ui.fatal "Invalid formation file path provided: #{Chef::Config[:knife][:cloudformation][:file]}"
+            exit 1
+          end
+        end
+
+        if(Chef::Config[:knife][:cloudformation][:template])
+          file = Chef::Config[:knife][:cloudformation][:template]
+        elsif(Chef::Config[:knife][:cloudformation][:processing])
+          file = SparkleFormation.compile(Chef::Config[:knife][:cloudformation][:file])
+        else
+          file = _from_json(File.read(Chef::Config[:knife][:cloudformation][:file]))
+        end
+        if(config[:print_only])
+          ui.warn 'Print only requested'
+          ui.info _format_json(file)
+          exit 1
+        end
+        ui.info "#{ui.color('Cloud Formation: ', :bold)} #{ui.color(action_type, :green)}"
+        stack_info = "#{ui.color('Name:', :bold)} #{name}"
+        if(Chef::Config[:knife][:cloudformation][:path])
+          stack_info << " #{ui.color('Path:', :bold)} #{Chef::Config[:knife][:cloudformation][:file]}"
+          if(Chef::Config[:knife][:cloudformation][:disable_processing])
+            stack_info << " #{ui.color('(not pre-processed)', :yellow)}"
+          end
+        end
+        ui.info "  -> #{stack_info}"
+        populate_parameters!(file)
+        stack_def = KnifeCloudformation::AwsCommons::Stack.build_stack_definition(file, Chef::Config[:knife][:cloudformation][:options])
+        create_stack(name, stack_def)
+      end
+
       def create_stack(name, stack)
         begin
           res = aws_con.update_stack(name, stack)
